c772427227dbc11654cb64e924130a573c2477cc
Dorin Matei <matei.dorin22@gmail.com>
2022-08-11 13:03:00 +0100

    created a function to POST requests

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,8 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get()
+    admin.firestore()
+    .collection('screams').get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
@@ -21,4 +22,26 @@ exports.getScreams = functions.https.onRequest((req, res) => {
             return res.json(screams);
         })
         .catch(err => console.error(err));
+})
+
+exports.createScream = functions.https.onRequest((req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(400).json({error: 'Method not allowed!!!'});
+    };
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAtd: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+        res.json({message: `document ${doc.id} created successfully`})
+    .catch(err => {
+        res.status(500).json({error: 'something went wrong'});
+        console.error(err);
+    })
+    })
 })
