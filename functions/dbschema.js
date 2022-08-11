e09586aac17e3f2694b0d39f656029cb857288e1
Dorin Matei <matei.dorin22@gmail.com>
2022-08-11 14:47:50 +0100

    working with express

---
+++ functions/dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2022-08-11T13:37:07.291Z'
+        }
+    ]
+}
